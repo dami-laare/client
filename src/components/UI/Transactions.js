9acaf3d4d9cfddba64e0609945158d373d3159cb
dami-laare <osunkiyesitayo@gmail.com>
2022-03-08 05:35:14 +0000

    hi

---
+++ src/components/UI/Transactions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Transactions = () => {
+  return (
+    <div className='mt-auto text-center transactions'>
+        <h3 className='f-600' style={{fontSize: '1.2rem'}}>Your Transactions</h3>
+        <p>No transactions yet.</p>
+    </div>
+  )
+}
+
+export default Transactions
